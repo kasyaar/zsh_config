c298e18a63aae083584e60be71f28842c019d5ab
Dmitry Kasimtsev <kasyaar@gmail.com>
2018-12-07 15:38:05 +0200

    adeed install envo

---
+++ instenvo.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+sudo apt-get install zsh vim git
+git clone https://github.com/kasyaar/oh-my-zsh .zsh
+ln -s ~/.zsh/zshrc .zshrc
+ln -s ~/.zsh/.gitconfig .gitconfig
+chsh -s /usr/bin/zsh
+
+git clone https://github.com/kasyaar/vim_config .vim
+ln -s ~/.vim/vimrc .vimhrc
+vim -c ":PlugInstall"
+
