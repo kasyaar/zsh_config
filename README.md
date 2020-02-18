837b2bf29b19b7b1f7d3e95d31c17a300b5e3bf5
Dmitry Kasimtsev <kasyaar@gmail.com>
2020-02-18 16:02:16 +0200

    updated by adding installation instructions

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# Install
+```shell
+git clone https://github.com/kasyaar/oh-my-zsh .zsh
+sh .zsh/prepare.sh <USER>
+sh .zsh/instenvo.sh```
 <p align="center">
   <img src="https://s3.amazonaws.com/ohmyzsh/oh-my-zsh-logo.png" alt="Oh My Zsh">
 </p>
--- instenvo.sh
+++ instenvo.sh
@@ -2,10 +2,11 @@
 sudo apt-get -y install zsh vim git
 git clone https://github.com/kasyaar/oh-my-zsh .zsh
 ln -s ~/.zsh/zshrc .zshrc
+ln -s ~/.zsh/.tmux.conf ~/.tmux.conf
 ln -s ~/.zsh/.gitconfig .gitconfig
 sudo chsh -s /usr/bin/zsh kasyaar
 
 git clone https://github.com/kasyaar/vim_config .vim
 ln -s ~/.vim/vimrc .vimhrc
-vim -c ":PlugInstall"
+vim -c ":PlugInstall :qall"
 
