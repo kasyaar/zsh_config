fd8520c4fedcacafa13db61c63f4b29f87057ec8
Dmitry Kasimtsev <kasyaar@gmail.com>
2020-03-12 14:14:00 +0200

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -2,5 +2,5 @@
 sudo useradd -m -G adm,sudo,netdev,users  ${1} 
 sudo mkdir /home/${1}/.ssh
 sudo chsh -s /bin/bash kasyaar
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCttvVVU1t1/OUmOs0VCLlxLjIkFe71BM3cDU4ILz0wj+b++ci7wyYze1koQ4yNWQCHPgRTr8QDZ0J88Xdwh6KFbo/MBJsA37sNIoEm521eiswHEpInP6LL5uCpjlOQWM0P/GsUOAsthiQVoWbe5yeVPGmbbc9BiIBEJvnZLyZki+X1Olja9zaZjhK+smTVDoU+McUyKm4qyngrd7mgT/zqn/xRGgRK2H+lsgg29oRVdHutFK0+H/Uy7gmsFgjk8O+a8Tt80SsJ54aRRTgb/LcDKI1dRkE7k+zeveuC9uR16FPbNruP+Dc4QL394GG7cJbgwS15Ix59ndjOSIC8rxll kasyaar@Dmitrys-MBP"|sudo tee /home/${1}/.ssh/authorized_keys
+echo ""|sudo tee /home/${1}/.ssh/authorized_keys
 echo "Don't forget to set user: ${1} password"
